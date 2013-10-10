3cfb1fcde26c6f54bae7a131c1d59f0b517eb17f
Ingo Walz <ingo.walz@googlemail.com>
2013-10-10 23:34:19 +0200

    Checkout in subdirectory

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 mkdir -p ~/.vim/autoload
 cd ~/.vim
-git clone git://github.com/iwalz/vim-c.git .
+git clone git://github.com/iwalz/vim-c.git vim-c
 curl 'www.vim.org/scripts/download_script.php?src_id=19375' > ~/.vim/autoload/pathogen.vim
-mv ./.vimrc ~/.vimrc
+mv vim-c/.vimrc ~/.vimrc
+cd vim-c
 git submodule init
 git submodule update
+cd ..
+mv vim-c/bundle bundle
+rm -rf vim-c
