2766c06abae4496e248d1e0115d2f51873eda9e9
Ingo Walz <ingo.walz@googlemail.com>
2013-10-13 16:32:50 +0200

    Add script for splice config

--- configure_vimdiff.sh
+++ configure_vimdiff.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-echo "[diff]" >> ~/.gitconfig
-echo "  external = ~/.vim/diff_wrapper" >> ~/.gitconfig
-wget -q https://raw.github.com/iwalz/vim-c/master/diff_wrapper -o ~/.vim/diff_wrapper
+echo "[merge]" >> ~/.gitconfig
+echo "  tool = splice" >> ~/.gitconfig
+echo "[mergetool \"splice\"]" >> ~/.gitconfig
+echo "  cmd = \"vim -f $BASE $LOCAL $REMOTE $MERGED -c 'SpliceInit'\"" >> ~/.gitconfig
+echo "  trustExitCode = true" >> ~/.gitconfig
